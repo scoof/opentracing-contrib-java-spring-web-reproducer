eb0035fe1715276730dce778ec8ddd2823424c2f
Andreas Plesner <apj@nerd.dk>
2019-07-02 10:48:27 +0200

    Reproduce

---
+++ src/test/java/dk/nerd/TestTest.java
@@ -0,0 +1,13 @@
+package dk.nerd;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@RunWith(SpringRunner.class)
+public class TestTest {
+  @Test
+  public void test() {}
+}
